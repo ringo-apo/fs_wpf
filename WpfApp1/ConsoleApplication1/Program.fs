5df24935030b2656cfa0f672b70bee9b596a83c8
taira_akira <taira_akira@nago_etech.local>
2020-04-09 18:09:49 +0900

    20200409

---
+++ WpfApp1/ConsoleApplication1/Program.fs
@@ -0,0 +1,14 @@
+﻿let buttonEventHandler (eventArgs: System.Windows.RoutedEventArgs) =
+    let button = eventArgs.Source :?> System.Windows.Controls.Button
+    System.Windows.MessageBox.Show(sprintf "%sからのイベントです" button.Name, "イベントハンドラのテスト")
+    |> ignore
+
+
+[<EntryPoint; System.STAThread>]
+let main _ =
+    let win = new WpfApp1.MainWindow()
+    win.Button1.Click
+    |> Observable.add buttonEventHandler
+
+    let app = new System.Windows.Application()
+    app.Run win
